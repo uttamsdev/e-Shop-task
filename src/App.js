6ed809c063e3b51dbc723370c8394c45316eee04
uttamsaha <uttamsaha982@live.com>
2022-11-25 16:52:14 +0600

    another prodcut section added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./Pages/Header/Header";
 import Banner from "./Pages/Banner/Banner";
 import Component from "./Pages/Component/Component";
+import HotDeal from "./Pages/HotDeal/HotDeal";
 
 function App() {
   return <div>
     <Header></Header>
     <Banner></Banner>
     <Component></Component>
+    <HotDeal></HotDeal>
   </div>;
 }
 
