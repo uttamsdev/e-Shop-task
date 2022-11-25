9f2829fbdee79f3e51016334927bd48f031a93a9
uttamsaha <uttamsaha982@live.com>
2022-11-25 16:44:34 +0600

    component section added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Pages/Header/Header";
 import Banner from "./Pages/Banner/Banner";
+import Component from "./Pages/Component/Component";
 
 function App() {
   return <div>
     <Header></Header>
     <Banner></Banner>
+    <Component></Component>
   </div>;
 }
 
---
+++ src/Pages/Component/Component.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Component.css'
+
+const Component = () => {
+  return (
+    <div className='container'>
+        <img className='c-img' src="component.png" alt="" />
+
+    </div>
+  )
+}
+
+export default Component;
